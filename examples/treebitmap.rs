0681cc71ef0aabc9361ed8de1308e0fba7133865
Jasper den Hertog <jasper@plainspace.com>
2021-02-23 10:14:51 +0100

    moar ips

--- examples/treebitmap.rs
+++ examples/treebitmap.rs
@@ -6,6 +6,8 @@ fn main() {
     let mut tree_bitmap = TreeBitMap::new();
     let pfxs = vec![
         Prefix::<u32, PrefixAs>::new(0b0000_0000_0000_0000_0000_0000_0000_0000_u32, 0),
+        Prefix::<u32, PrefixAs>::new(0b1111_1111_1111_1111_1111_1111_1111_1111_u32, 32),
+        Prefix::new(0b0000_0000_0000_0000_0000_0000_0000_0000_u32, 4),
         Prefix::new(0b0001_0000_0000_0000_0000_0000_0000_0000_u32, 4),
         Prefix::new(0b0010_0000_0000_0000_0000_0000_0000_0000_u32, 4),
         Prefix::new(0b0011_0000_0000_0000_0000_0000_0000_0000_u32, 4),
@@ -19,7 +21,7 @@ fn main() {
         Prefix::new(0b1100_0000_0000_0000_0000_0000_0000_0000_u32, 4),
         Prefix::new(0b1101_0000_0000_0000_0000_0000_0000_0000_u32, 4),
         Prefix::new(0b1110_0000_0000_0000_0000_0000_0000_0000_u32, 4),
-        Prefix::new(0b0111_0000_0000_0000_0000_0000_0000_0000_u32, 4),
+        Prefix::new(0b1111_0000_0000_0000_0000_0000_0000_0000_u32, 4),
         Prefix::new(0b1111_0000_0000_0000_0000_0000_0000_0000_u32, 9),
         Prefix::new(0b1111_0000_1000_0000_0000_0000_0000_0000_u32, 9),
         Prefix::<u32, PrefixAs>::new(0b1111_0000_0000_0000_0000_0000_0000_0000_u32, 9),
@@ -42,15 +44,14 @@ fn main() {
         Prefix::<u32, PrefixAs>::new(std::net::Ipv4Addr::new(193, 0, 0, 0).into(), 16),
         Prefix::<u32, PrefixAs>::new(std::net::Ipv4Addr::new(193, 0, 10, 0).into(), 23),
         Prefix::<u32, PrefixAs>::new(std::net::Ipv4Addr::new(193, 0, 9, 0).into(), 24),
-        // Prefix::<u32, PrefixAs>::new(std::net::Ipv4Addr::new(193, 0, 10, 0).into(), 24),
-
-        // Prefix::<u32, PrefixAs>::new(0b0011_0000_0000_0000_0000_0000_0000_0000_u32, 4),
-        // Prefix::<u32, PrefixAs>::new(0b1000_0011_1000_1111_0000_0000_0000_0000_u32, 11),
-        // Prefix::<u32, PrefixAs>::new(0b1000_0010_0101_0111_1111_1000_0000_0000_u32, 13),
-        // Prefix::new(std::net::Ipv4Addr::new(130, 55, 240, 0).into(), 24),
-        // Prefix::<u32, PrefixAs>::new(0b1111_1111_0000_0001_0000_0000_0000_0000_u32, 12),
-        // Prefix::<u32, PrefixAs>::new(0b1111_1111_0011_0111_0000_0000_0000_0000_u32, 17),
-        // Prefix::<u32, PrefixAs>::new(std::net::Ipv4Addr::new(100, 0, 12, 0).into(), 24),
+        Prefix::<u32, PrefixAs>::new(std::net::Ipv4Addr::new(193, 0, 10, 0).into(), 24),
+        Prefix::<u32, PrefixAs>::new(0b0011_0000_0000_0000_0000_0000_0000_0000_u32, 4),
+        Prefix::<u32, PrefixAs>::new(0b1000_0011_1000_1111_0000_0000_0000_0000_u32, 11),
+        Prefix::<u32, PrefixAs>::new(0b1000_0010_0101_0111_1111_1000_0000_0000_u32, 13),
+        Prefix::new(std::net::Ipv4Addr::new(130, 55, 240, 0).into(), 24),
+        Prefix::<u32, PrefixAs>::new(0b1111_1111_0000_0001_0000_0000_0000_0000_u32, 12),
+        Prefix::<u32, PrefixAs>::new(0b1111_1111_0011_0111_0000_0000_0000_0000_u32, 17),
+        Prefix::<u32, PrefixAs>::new(std::net::Ipv4Addr::new(100, 0, 12, 0).into(), 24),
     ];
 
     for pfx in pfxs.iter() {
@@ -61,8 +62,8 @@ fn main() {
     // println!("pfxbitarr: {:032b}", tree_bitmap.0.pfxbitarr);
 
     for spfx in &[
-        // Prefix::new(0b0000_0000_0000_0000_0000_0000_0000_0000_u32, 0),
-        // Prefix::new(0b0000_0000_0000_0000_0000_0000_0000_0000_u32, 1),
+        Prefix::new(0b0000_0000_0000_0000_0000_0000_0000_0000_u32, 0),
+        Prefix::new(0b0000_0000_0000_0000_0000_0000_0000_0000_u32, 4),
         Prefix::new(0b0001_0000_0000_0000_0000_0000_0000_0000_u32, 4),
         Prefix::new(0b0010_0000_0000_0000_0000_0000_0000_0000_u32, 4),
         Prefix::new(0b0011_0000_0000_0000_0000_0000_0000_0000_u32, 4),
@@ -78,7 +79,6 @@ fn main() {
         Prefix::new(0b1110_0000_0000_0000_0000_0000_0000_0000_u32, 4),
         Prefix::new(0b0111_0000_0000_0000_0000_0000_0000_0000_u32, 4),
         Prefix::new(0b1111_0000_0000_0000_0000_0000_0000_0000_u32, 9),
-        Prefix::new(0b1111_0000_1000_0000_0000_0000_0000_0000_u32, 9),
         Prefix4::new(std::net::Ipv4Addr::new(255, 1, 0, 0).into(), 24),
         Prefix4::new(std::net::Ipv4Addr::new(224, 0, 0, 0).into(), 4),
         Prefix4::new(std::net::Ipv4Addr::new(224, 0, 0, 0).into(), 8),
@@ -90,7 +90,7 @@ fn main() {
         Prefix::new(std::net::Ipv4Addr::new(193, 0, 3, 0).into(), 23),
         Prefix::new(std::net::Ipv4Addr::new(193, 0, 10, 0).into(), 23),
         Prefix::new(std::net::Ipv4Addr::new(193, 0, 10, 0).into(), 24),
-        // Prefix::new(std::net::Ipv4Addr::new(100, 0, 12, 0).into(), 24),
+        Prefix::new(std::net::Ipv4Addr::new(100, 0, 12, 0).into(), 24),
     ] {
         println!("search for: {:?}", spfx);
         let s_spfx = tree_bitmap.match_longest_prefix(&spfx);
