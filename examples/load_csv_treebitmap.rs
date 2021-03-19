1daf67612b74d0d5400a09c177411ee9bb706d4c
Jasper den Hertog <jasper@plainspace.com>
2021-03-19 09:51:11 +0100

    async prefix loading

--- examples/load_csv_treebitmap.rs
+++ examples/load_csv_treebitmap.rs
@@ -2,10 +2,10 @@ use std::env;
 use std::error::Error;
 use std::ffi::OsString;
 use std::fs::File;
-use std::process;
 use trie::common::{NoMeta, Prefix, PrefixAs};
 use trie::treebitmap::TreeBitMap;
 
+use async_channel;
 use shrust::{Shell, ShellIO};
 use std::io::prelude::*;
 
@@ -16,64 +16,97 @@ fn get_first_arg() -> Result<OsString, Box<dyn Error>> {
     }
 }
 
-fn load_prefixes(pfxs: &mut Vec<Prefix<u32, PrefixAs>>) -> Result<(), Box<dyn Error>> {
+async fn load_prefixes(
+    tx: async_channel::Sender<Prefix<u32, PrefixAs>>,
+) -> Result<(), Box<dyn Error>> {
     // Build the CSV reader and iterate over each record.
     let file_path = get_first_arg()?;
     let file = File::open(file_path)?;
     let mut rdr = csv::Reader::from_reader(file);
+
     for result in rdr.records() {
-        // The iterator yields Result<StringRecord, Error>, so we check the
-        // error here.
-        let record = result?;
-        // let pfx = Prefix::<u32, PrefixAs>::new_with_meta(net, len, asn);
-        let ip: Vec<_> = record[0]
-            .split(".")
-            .map(|o| -> u8 { o.parse().unwrap() })
-            .collect();
-        let net = std::net::Ipv4Addr::new(ip[0], ip[1], ip[2], ip[3]);
-        let len: u8 = record[1].parse().unwrap();
-        let asn: u32 = record[2].parse().unwrap();
-        let pfx = Prefix::<u32, PrefixAs>::new_with_meta(net.into(), len, PrefixAs(asn));
-        pfxs.push(pfx);
-        // trie.insert(&pfx);
-        // println!("{:?}", pfx);
+        let tx = tx.clone();
+        tokio::spawn(async move {
+
+            let record = result.unwrap();
+            let ip: Vec<_> = record[0]
+                .split(".")
+                .map(|o| -> u8 { o.parse().unwrap() })
+                .collect();
+            let net = std::net::Ipv4Addr::new(ip[0], ip[1], ip[2], ip[3]);
+            let len: u8 = record[1].parse().unwrap();
+            let asn: u32 = record[2].parse().unwrap();
+            let pfx = Prefix::<u32, PrefixAs>::new_with_meta(net.into(), len, PrefixAs(asn));
+
+            match tx.send(pfx).await {
+                Ok(_) => {
+                    // print!(".");
+                }
+                Err(err) => {
+                    println!("err {:?} {}", err.into_inner(), tx.is_closed());
+                }
+            };
+        });
     }
     Ok(())
 }
 
-fn main() {
-    let mut pfxs: Vec<Prefix<u32, PrefixAs>> = vec![];
+#[tokio::main(flavor = "multi_thread", worker_threads = 10)]
+async fn main() {
     let mut tree_bitmap: TreeBitMap<u32, PrefixAs> = TreeBitMap::new();
+    println!("start loading prefixes...");
+    let start = std::time::Instant::now();
 
-    if let Err(err) = load_prefixes(&mut pfxs) {
-        println!("error running example: {}", err);
-        process::exit(1);
-    }
-    println!("finished loading {} prefixes...", pfxs.len());
-    for pfx in pfxs.into_iter() {
-        tree_bitmap.insert(pfx);
-    }
+    let (tx, rx) = async_channel::bounded(10000);
 
+    let store_tree = tokio::spawn(async move {
+        let mut duplicate_cnt: usize = 0;
+        loop {
+            let rx = rx.clone();
+            match rx.recv().await {
+                Ok(pfx) => {
+                    duplicate_cnt += !tree_bitmap.insert(pfx) as usize;
+                }
+                Err(err) => {
+                    if rx.is_closed() {
+                        println!("channel closed...");
+                        println!("found {} duplicate prefixes", duplicate_cnt);
+                        return tree_bitmap;
+                    }
+                    else {
+                        panic!("channel crashed: {:?}", err);
+                    }
+                }
+            }
+        }
+    });
+
+    load_prefixes(tx).await.unwrap();
+    let tree_bitmap = store_tree.await.unwrap();
+
+    let ready = std::time::Instant::now();
     println!("prefix vec size {}", tree_bitmap.prefixes.len());
-    println!("finished building tree...");
-    // println!(
-    //     "{:?} nodes created",
-    //     tree_bitmap.stats.iter().fold(0, |mut acc, c| {
-    //         acc += c.created_nodes.iter().fold(0, |mut sum, l| {
-    //             sum += l.count;
-    //             sum
-    //         });
-    //         acc
-    //     })
-    // );
-    println!("stride division  {:?}", TreeBitMap::<u32, PrefixAs>::STRIDES);
+    println!(
+        "finished building tree in {} msecs...",
+        ready.checked_duration_since(start).unwrap().as_millis()
+    );
+    println!(
+        "{:?} nodes created in total",
+        tree_bitmap.stats.iter().fold(0, |mut acc, c| {
+            acc += c.created_nodes.iter().fold(0, |mut sum, l| {
+                sum += l.count;
+                sum
+            });
+            acc
+        })
+    );
+    println!(
+        "stride division  {:?}",
+        TreeBitMap::<u32, PrefixAs>::STRIDES
+    );
     for s in &tree_bitmap.stats {
         println!("{:?}", s);
     }
-
-    let spfx = Prefix::new(std::net::Ipv4Addr::new(193, 0, 10, 0).into(), 23);
-    let fpfx = tree_bitmap.match_longest_prefix(&spfx);
-    println!("search for: {:?}, found {:?}", spfx, fpfx);
 
     let mut shell = Shell::new(tree_bitmap);
     shell.new_command("s", "search the RIB", 1, |io, tree_bitmap, s| {
@@ -93,28 +126,4 @@ fn main() {
     });
 
     shell.run_loop(&mut ShellIO::default());
-
-    // let spfx = Prefix::<u32, NoMeta>::new(std::net::Ipv4Addr::new(7, 0, 0, 0).into(), 22);
-    // let fpfx = trie.match_longest_prefix(&spfx);
-    // println!("search for: {:?}, found {:?}", spfx, fpfx);
-
-    // let spfx = Prefix::<u32, NoMeta>::new(std::net::Ipv4Addr::new(103, 108, 187, 247).into(), 24);
-    // let fpfx = trie.match_longest_prefix(&spfx);
-    // println!("search for: {:?}, found {:?}", spfx, fpfx);
-
-    // let spfx = Prefix::<u32, NoMeta>::new(std::net::Ipv4Addr::new(103, 108, 187, 247).into(), 23);
-    // let fpfx = trie.match_longest_prefix(&spfx);
-    // println!("search for: {:?}, found {:?}", spfx, fpfx);
-
-    // let spfx = Prefix::<u32, NoMeta>::new(std::net::Ipv4Addr::new(214,10,23,0).into(), 24);
-    // let fpfx = trie.match_longest_prefix(&spfx);
-    // println!("search for: {:?}, found {:?}", spfx, fpfx);
-
-    // let spfx = Prefix::<u32, NoMeta>::new(std::net::Ipv4Addr::new(214,10,23,0).into(), 0);
-    // let fpfx = trie.match_longest_prefix(&spfx);
-    // println!("search for: {:?}, found {:?}", spfx, fpfx);
-
-    // let spfx = Prefix::<u32, NoMeta>::new(std::net::Ipv4Addr::new(0,0,0,0).into(), 0);
-    // let fpfx = trie.match_longest_prefix(&spfx);
-    // println!("search for: {:?}, found {:?}", spfx, fpfx);
 }
