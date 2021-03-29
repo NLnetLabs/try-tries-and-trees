5e4286277f8106021034430f7e0e6dffd7c4afbd
Jasper den Hertog <jasper@plainspace.com>
2021-03-29 09:50:27 +0200

    be gone all print

--- src/radixtrie/mod.rs
+++ src/radixtrie/mod.rs
@@ -59,24 +59,13 @@ where
         let zero = num::zero();
 
         loop {
-            // print!("*bp{}", cursor.bit_pos);
-            // println!("cursor#1 {:#?}", cursor);
-
             // we might already be at the place we need to be,
             // at either a leaf or an internal node, just
             // set our insert prefix here and be done with it.
             if pfx.len == cursor.bit_pos {
-                // println!("cursor {:?}/{:?}", cursor.bit_id, cursor.bit_pos);
                 cursor.prefix = Some(&pfx);
                 break;
             }
-
-            // println!("prefix {:?}", pfx);
-            // println!(
-            //     "test bitpos {} {:032b}",
-            //     cursor.bit_pos,
-            //     pfx.net << (cursor.bit_pos as usize - 1)
-            // );
 
             match (pfx.net << cursor.bit_pos as usize) & AF::BITMASK {
                 // Inspected bit is ZERO, this node should go to the LEFT
@@ -85,24 +74,19 @@ where
                         // No node on the left, so we can create a new leaf node
                         // with our prefix length and the prefix.
                         None => {
-                            // print!("+l{}", pfx.len);
                             let mut new_leaf = RadixTrieNode::new(Some(&pfx));
                             new_leaf.bit_pos = pfx.len;
                             new_leaf.bit_id = pfx.net >> (AF::BITS - pfx.len) as usize;
                             cursor.left = Some(Box::new(new_leaf));
-                            // println!("bit_id {:032b}", pfx.net >> (AF::BITS - pfx.len) as usize);
                             break;
                         }
                         // There is a node on the left side, we need to see if that node's
                         // bit_id matches our bit_id.
                         Some(next_node) => {
-                            // println!("=l");
-                            // println!("{:#?}", cursor);
                             // The prefix is aligned with the next_node, move on
                             if pfx.net >> (AF::BITS - next_node.bit_pos) as usize
                                 == next_node.bit_id
                             {
-                                // println!("<-<-<-turn Left");
                             } else {
                                 // figure out where a possible new intermediary node should be placed
                                 // , by comparing the new intermediary node's prefix with the next
@@ -132,36 +116,6 @@ where
                                 intermediary_node.bit_pos = in_bit_pos;
                                 intermediary_node.bit_id = in_bit_id;
 
-                                // println!(
-                                //     "pfx.len {:2} pfx.net          {:032b}",
-                                //     &pfx.len, &pfx.net
-                                // );
-                                // println!(
-                                //     "next_node bit_pos {:2} bit_id {:032b}",
-                                //     &next_node.bit_pos, &next_node.bit_id
-                                // );
-                                // println!(
-                                //     "shift next_node             {:032b}",
-                                //     next_node.bit_id << (AF::BITS - next_node.bit_pos) as usize
-                                // );
-                                // println!(
-                                //     "XOR                         {:032b}",
-                                //     pfx.net
-                                //         ^ (next_node.bit_id
-                                //             << (AF::BITS - next_node.bit_pos) as usize)
-                                // );
-                                // println!(
-                                //     "interm bit_pos {:2} bit_id  {:032b}",
-                                //     intermediary_node.bit_pos, intermediary_node.bit_id
-                                // );
-                                // println!(
-                                //     "leading zeros {}",
-                                //     (pfx.net
-                                //         ^ (next_node.bit_id
-                                //             << (AF::BITS - next_node.bit_pos) as usize))
-                                //         .leading_zeros()
-                                // );
-
                                 // If we've reached the pfx.len in the intermediary_node,
                                 // that means that it will have to host a prefix, (so
                                 // it'll become a regular internal node).
@@ -181,16 +135,6 @@ where
                                     << ((AF::BITS - next_node.bit_pos) + intermediary_node.bit_pos)
                                         as usize;
                                 let l_r_bit_next_node = ident.leading_zeros() == 0;
-                                // println!("next_node bit_id {:032b}", next_node.bit_id);
-                                // println!(
-                                //     "shift            {:032b}",
-                                //     next_node.bit_id
-                                //         << ((AF::BITS - next_node.bit_pos)
-                                //             + intermediary_node.bit_pos)
-                                //             as usize
-                                // );
-                                // println!("dir -><- {:?}", l_r_bit_next_node);
-                                // print!("ident {:?}", ident);
 
                                 if in_bit_pos < pfx.len {
                                     match l_r_bit_next_node {
@@ -232,7 +176,6 @@ where
                                     }
 
                                     if done {
-                                        // print!("D");
                                         break;
                                     }
                                 }
@@ -266,7 +209,6 @@ where
                 _ => {
                     match &cursor.right {
                         None => {
-                            // print!("+r{}", pfx.len);
                             let mut nn = RadixTrieNode::new(Some(&pfx));
                             nn.bit_pos = pfx.len;
                             nn.bit_id = pfx.net >> (AF::BITS - pfx.len) as usize;
@@ -274,13 +216,9 @@ where
                             break;
                         }
                         Some(next_node) => {
-                            // println!("=r");
-
                             if pfx.net >> (AF::BITS - next_node.bit_pos) as usize
                                 == next_node.bit_id
                             {
-                                // println!("->->->Right");
-                                // println!("{:#?}", cursor);
                             } else {
                                 let in_bit_pos = (pfx.net
                                     ^ (next_node.bit_id << (AF::BITS - next_node.bit_pos) as usize))
@@ -298,7 +236,7 @@ where
                                     << ((AF::BITS - next_node.bit_pos) + intermediary_node.bit_pos)
                                         as usize;
                                 let l_r_bit_next_node = ident.leading_zeros() == 0;
-                                
+
                                 // If we've reached the pfx.len in the intermediary_node,
                                 // that means that it will have to host a prefix, (so
                                 // it'll become a regular internal node).
@@ -315,7 +253,6 @@ where
                                         // Our prefix should go the right, so move the
                                         // existing one to the left.
                                         false => {
-                                            // print!("->");
                                             intermediary_node.left =
                                                 std::mem::take(&mut cursor.right);
                                             if !done {
@@ -329,7 +266,6 @@ where
                                             cursor.right = Some(Box::new(intermediary_node));
                                         }
                                         true => {
-                                            // print!("<-");
                                             intermediary_node.right =
                                                 std::mem::take(&mut cursor.right);
                                             if !done {
@@ -347,7 +283,6 @@ where
                                 } else {
                                     match l_r_bit_next_node {
                                         true => {
-                                            // print!(";->;");
                                             let mut nn = RadixTrieNode::new(Some(&pfx));
                                             nn.bit_pos = pfx.len;
                                             nn.bit_id = pfx.net >> (AF::BITS - pfx.len) as usize;
@@ -356,7 +291,6 @@ where
                                             break;
                                         }
                                         false => {
-                                            // print!(";<-;");
                                             let mut nn = RadixTrieNode::new(Some(&pfx));
                                             nn.bit_pos = pfx.len;
                                             nn.bit_id = pfx.net >> (AF::BITS - pfx.len) as usize;
@@ -376,19 +310,6 @@ where
                 }
             }
         }
-
-        // let len = pfx.len;
-        // let shift: usize = (AF::BITS - pfx.len) as usize;
-        // let net = built_prefix << if shift < AF::BITS as usize { shift } else { 0 };
-
-        // println!(
-        //     "S inserted prefix: {:?} -> {:?}/{}",
-        //     pfx,
-        //     AF::fmt_net(net),
-        //     len
-        // );
-
-        // println!("cursor {:#?}", cursor);
     }
 
     pub fn match_longest_prefix(
@@ -401,26 +322,20 @@ where
 
         let mut next_pos = search_pfx.net << cursor.unwrap().bit_pos as usize;
         loop {
-            // print!("*bp{}", cursor.unwrap().bit_pos);
             let bit_id_match = (search_pfx.net
                 ^ (cursor.unwrap().bit_id << (AF::BITS - cursor.unwrap().bit_pos) as usize))
                 .leading_zeros()
                 >= cursor.unwrap().bit_pos as u32;
-          
 
             match next_pos & AF::BITMASK {
                 b if b == zero && bit_id_match => {
-                    // print!("l");
                     cursor = cursor.and_then(|c| c.left.as_deref()).and_then(|c| {
-                        // println!("L {} less specific : {:?}", c.bit_pos, c.prefix);
                         match_pfx = c.prefix;
                         Some(c)
                     });
-                }   
+                }
                 _ if bit_id_match => {
-                    // print!("r");
                     cursor = cursor.and_then(|c| c.right.as_deref()).and_then(|c| {
-                        // println!("R {} less specific : {:?}", c.bit_pos, c.prefix);
                         match_pfx = c.prefix;
                         Some(c)
                     });
